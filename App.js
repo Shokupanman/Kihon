5df9d03a120b86f0c2be7650f9d0b486f68167ee
Shokupanman <Harrisonkaidehancock@gmail.com>
2020-05-28 11:38:35 +0900

    basic testing done

--- App.js
+++ App.js
@@ -1,26 +1,48 @@
 import * as React from 'react'
 import { Platform, StyleSheet, Text, View, Image } from 'react-native'
-import { Card, Title, Paragraph } from 'react-native-paper'
-import logo from './img/logo.png'
+import { Button, Card, Title, Paragraph } from 'react-native-paper'
 
 export default function App() {
   return (
-    <View style={styles.header}>
-      <Image source={logo} sytle={styles.tinyLogo} />
+    <View>
+      <View style={styles.iphoneTop}></View>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Kiso (Logo will go here)</Text>
+      </View>
+      <Card style={styles.card}>
+        <Card.Title title="Testi Card" subtitle="How developement feels" />
+        <Card.Content></Card.Content>
+        <Card.Cover
+          source={{
+            uri:
+              'https://images.unsplash.com/photo-1507808973436-a4ed7b5e87c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80',
+          }}
+        />
+        <Card.Actions>
+          <Button>Start Lesson</Button>
+        </Card.Actions>
+      </Card>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  iphoneTop: {
+    backgroundColor: '#e74b5e',
+    height: 18,
+  },
   header: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#D01C30',
-    height: '25%',
+    backgroundColor: '#E43A4D',
+    height: 45,
   },
-  tinyLogo: {
-    width: 50,
-    height: 50,
+  headerText: {
+    color: '#FBFEF9',
+    fontSize: 20,
+  },
+  card: {
+    margin: 10,
   },
 })
